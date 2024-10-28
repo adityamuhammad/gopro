f3a8daf1f3d40a95e844fa4ee3bff7be6a47d5a3
Aditya <aditya210muhammad@gmail.com>
2024-10-28 14:39:37 +0800

    add asynq lib to handle queue background task

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -3,19 +3,27 @@ package handlers
 import (
 	"gopro/config"
 	"gopro/models"
+	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"gopro/tasks"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/hibiken/asynq"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
+const redisAddr = "127.0.0.1:6379"
+
 func Register(c *gin.Context) {
 	var user models.User
+	client := asynq.NewClient(asynq.RedisClientOpt{Addr: redisAddr})
+	defer client.Close()
 
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -27,6 +35,16 @@ func Register(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 		return
 	}
+
+	task, err := tasks.NewEmailDeliveryTask(42, "some:template:id")
+	if err != nil {
+		log.Fatalf("could not create task: %v", err)
+	}
+	info, err := client.Enqueue(task)
+	if err != nil {
+		log.Fatalf("could not enqueue task: %v", err)
+	}
+	log.Printf("enqueued task: id=%s queue=%s", info.ID, info.Queue)
 
 	user.Password = string(hashedPassword)
 	config.DB.Create(&user)
